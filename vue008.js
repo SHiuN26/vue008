d2dc9e43fdb02972e6849cc64c265ba8cfe3f176
SHiuN26 <justodme26@gmail.com>
2021-12-23 19:55:20 +0800

    3

--- vue008.js
+++ vue008.js
@@ -11,7 +11,10 @@ $(document).ready(function () {
     var vm = new Vue({
         el: "#app",
         data: {
-            sounddata: soundpack
+            sounddata: soundpack,
+            notes: [{num: 3,time: 105},{num:3,time:223},{num:4,time:331},{num:5,time:482},{num:5,time:565},{num:4,time:673},{num:3,time:782},{num:2,time:893},{num:1,time:1006},{num:1,time:1113},{num:2,time:1220},{num:3,time:1337},{num:3,time:1456},{num:2,time:1623},{num:2,time:1680},{num:3,time:1883},{num:3,time:1988},{num:4,time:2107},{num:5,time:2218},{num:5,time:2337},{num:4,time:2446},{num:3,time:2555},{num:2,time:2664},{num:1,time:2771},{num:1,time:2880},{num:2,time:2992},{num:3,time:3103},{num:2,time:3220},{num:1,time:3395},{num:1,time:3449}],
+            now_note_id: 0
+
         },
         methods: {
             playnote: function(id,volume){
@@ -21,6 +24,11 @@ $(document).ready(function () {
                     audio_obj.currentTime=0;
                     audio_obj.play();
                 }
+            },
+            playnext: function(volume){
+                var play_note=this.notes[this.now_note_id].num;
+                this.playnote(play_note,volume);
+                this.now_note_id+=1;
             }
         }
     });
